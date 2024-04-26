a6e37e23acd9581eee07a7aee019d9033884461b
Werner Van Geit <werner.vangeit@epfl.ch>
2024-04-26 11:29:39 +0200

    Add log message when retrying file write

--- src/handshakers.py
+++ src/handshakers.py
@@ -64,6 +64,7 @@ class FileHandshaker:
         if self.last_write is not None:
             path, content = self.last_write
             self.write_filecontent(path, content)
+            logger.info(f"Retried writingfile content to {path}")
 
     def shake_initiator(self):
         """Shake hand by initiator"""
