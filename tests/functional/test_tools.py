39a8756deca68e17179b22bab916bdefd611ecff
Werner Van Geit <werner.vangeit@epfl.ch>
2024-11-22 16:03:13 +0100

    Remove some commented out code

--- tests/functional/test_tools.py
+++ tests/functional/test_tools.py
@@ -6,7 +6,6 @@ from osparc_filecomms import tools
 
 
 def test_read_json(tmp_path):
-
     test_json_path = tmp_path / "test.json"
 
     test_json_data = {"test": "data"}
@@ -14,8 +13,8 @@ def test_read_json(tmp_path):
 
     assert tools.load_json(test_json_path, wait=False) == test_json_data
 
-def test_wait_for_path(tmp_path):
 
+def test_wait_for_path(tmp_path):
     test_path = tmp_path / "test.json"
 
     executor = futures.ProcessPoolExecutor(max_workers=1)
@@ -29,32 +28,7 @@ def test_wait_for_path(tmp_path):
     test_path.write_text("test")
 
     time.sleep(1)
-    
-    assert wait_future.done()
-    
-    executor.shutdown(wait=False, cancel_futures=True)
 
-    # test_json_data = {"test": "data"}
-    # test_json_path.write_text(json.dumps(test_json_data))
-    #
-    # assert tools.load_json(test_json_path, wait=False) == test_json_data
-    
-    # test_input_dir = tmp_path / "test_input_dir"
-    # test_output_dir = tmp_path / "test_output_dir"
-    # test_input_dir.mkdir()
-    # test_output_dir.mkdir()
-    #
-    # initiator_uuid = str(uuid.uuid4())
-    # receiver_uuid = str(uuid.uuid4())
-    #
-    # initiator_other_uuid = executor.submit(
-    #     run_handshake, initiator_uuid, test_input_dir, test_output_dir, True
-    # )
-    # receiver_other_uuid = executor.submit(
-    #     run_handshake, receiver_uuid, test_output_dir, test_input_dir, False
-    # )
-    #
-    # executor.shutdown()
-    #
-    # assert initiator_other_uuid.result() == receiver_uuid
-    # assert receiver_other_uuid.result() == initiator_uuid
+    assert wait_future.done()
+
+    executor.shutdown(wait=False, cancel_futures=True)
